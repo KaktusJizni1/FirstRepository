53ba0db0de94d778d1cd2949808e726f78900564
RomaSh <romashunkarenko@gmail.com>
2025-12-07 17:34:09 +0200

    update

--- script.js
+++ script.js
@@ -21,15 +21,6 @@ menu.addEventListener('click', function(event){
     }
 }});
 
-menu.addEventListener('click', function(event){
-   let clickTarget = event.target;
-   let activeBtn = document.querySelector('.active');
-   if (clickTarget.classList.contains('nav-link') ){
-       clickTarget.classList.add('active');
-       activeBtn.classList.remove('active');
-
-   }
-});
 
 let classLink = '.main-link';
 window.onscroll = function() {
